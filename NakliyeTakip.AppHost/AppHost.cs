a3ccba62e6e210254a52d4887a5fcb8be252a23b
Fatih Demir <fatihdemirtc@gmail.com>
2025-11-15 13:45:53 +0300

    Refactor app structure and update configurations
    Simplified `AppHost.cs` by removing specific project additions
    and directly building and running the application.
    
    Added HTTPS profile to `launchSettings.json` with updated
    `applicationUrl` and environment variables. Changed HTTP
    port from `5020` to `5142`.
    
    Updated `NakliyeTakip.Location.API.http` to reflect the new
    host address. Modified `Program.cs` to make `WeatherForecast`
    an internal record and added a `TemperatureF` property.
    
    Replaced the project GUID for `NakliyeTakip.Location.API` in
    `NakliyeTakip.sln` and updated references in solution
    configuration and nested project mappings.
    
    These changes improve maintainability, enhance security
    with HTTPS, and ensure consistency across the solution.

--- NakliyeTakip.AppHost/AppHost.cs
+++ NakliyeTakip.AppHost/AppHost.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.NakliyeTakip_API>("nakliyetakip-api");
 
 builder.AddProject<Projects.NakliyeTakip_Location_API>("nakliyetakip-location-api");
+
 
 builder.Build().Run();
